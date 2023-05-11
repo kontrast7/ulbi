bcb5b0ffad547323f11169e6a678eeb948bdb58f
Maksim Mashko <maksim.m@brlab.dev>
2023-05-11 19:40:15 +0200

    six lession

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,8 @@ import { BuildOptions } from "./types/config";
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
-    open: true,
+    open: false,
+    // * Делает доступными разу все роуты, а не только через главный роут (/)
+    historyApiFallback: true,
   };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { Counter } from "./components/Counter";
+import React, { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import "./index.scss";
 
 const App = () => {
   return (
     <div className="app">
-      123
-      <Counter />
+      <Link to="/about">AboutPage</Link>
+      <Link to="/">MainPage</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageLazy />} />
+          <Route path="/about" element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
